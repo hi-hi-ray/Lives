e3bdbda9160fb8485f2cbaf5fcb6d826e73161cf
hi-hi-ray <dutra.raysa@gmail.com>
2020-01-05 21:29:55 -0300

    Live #1 - Conhecendo Dart

---
+++ iniciando-em-dart/lib/iniciando_em_dart.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library iniciando_em_dart;
+
+export 'src/iniciando_em_dart_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
