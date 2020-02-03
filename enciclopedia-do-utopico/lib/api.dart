0ee0dfc855763028b8eb1c081b0e8cce4acf78f7
hi-hi-ray <dutra.raysa@gmail.com>
2020-02-02 21:13:53 -0300

    cria arquivo parar fazer a api

---
+++ enciclopedia-do-utopico/lib/api.dart
@@ -0,0 +1,17 @@
+import 'package';
+import 'package:http/http.dart' as http;
+
+// TODO
+// 1 - Criar um método de rotas.
+// 2 - Criar um método de envio.
+// 3 - Criar um método de response.
+//API RESTFULLLLLLL!
+// métodos http
+// get
+// get/id
+// post
+// put
+// delete
+// delete/id
+// patch
+
